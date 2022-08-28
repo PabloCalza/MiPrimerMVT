b6207300a6e60a21c610309452542f6119c4803c
Pablo Calza <pablocalza2@gmail.com>
2022-08-27 21:54:01 -0300

    mi primer mvt

---
+++ AppMiPrimerMVT/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Familiares
+from django.http import HttpResponse
+# Create your views here.
+
+def familiares(request):
+    familia1=Familiares(nombre="Guillermo", edad=64, fecha_de_nacimiento="1957-10-01")
+    familia1.save()
+    familia2=Familiares(nombre="Patricia", edad=68, fecha_de_nacimiento="1954-09-13")
+    familia2.save()
+    familia3=Familiares(nombre="Mercedes", edad=38, fecha_de_nacimiento="1983-11-11")
+    familia3.save()
+    texto=f"Familiar 1: nombre: {familia1.nombre} edad: {familia1.edad} fecha de nacimiento: {familia1.fecha_de_nacimiento}.  Familiar 2: nombre: {familia2.nombre} edad: {familia2.edad} fecha de nacimiento: {familia2.fecha_de_nacimiento} Familiar 3: nombre: {familia3.nombre} edad:{familia3.edad} fecha de nacimiento: {familia3.fecha_de_nacimiento}"
+    return HttpResponse (texto)
